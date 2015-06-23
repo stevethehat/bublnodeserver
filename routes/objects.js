c5fbe89ad056ad059be00ab23ed25c8829220d36
steve work <stevework@nicks-iphone.gateway.2wire.net>
2015-06-23 14:06:30 +0100

    added basic listing and object view functionality

--- routes/objects.js
+++ routes/objects.js
@@ -1,9 +1,78 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("mongodb");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+var connectionString = 'mongodb://bublUser:bublUser@analyticstestvm.cloudapp.net:31031/bublv2';
+
+function getCollection(callback){
+	var client = mongo.MongoClient;
+	client.connect(connectionString,
+		function(error, db){
+			if(error){
+				console.log(error);
+			} else {
+				console.log('we have a db' + db);
+				var collection = db.collection('objects');
+				callback(collection);	
+			}
+		}
+	);
+}
+
+router.get('/', function(request, response, next) {
+	getCollection(
+		function(collection){
+			console.log('got collection ' + collection);
+			collection.find().toArray(
+				function(error, results){
+					if(error){
+						
+					} else {
+						response.send(JSON.stringify(results, null, 4));
+					}					
+				}	
+			);
+		}
+	);
+});
+
+router.get('/:object_id', function(request, response, next) {
+	getCollection(
+		function(collection){
+			console.log('got collection ' + collection);
+			collection.find({ 'id': request.params.object_id }).toArray(
+				function(error, results){
+					response.send(JSON.stringify(results[0]));
+				}
+			);
+		}
+	);
+});
+
+router.get('/:object_id/getchildren', function(request, response, next) {
+	getCollection(
+		function(collection){
+			console.log('got collection ' + collection);
+			collection.find({ 'id': request.params.object_id }).toArray(
+				function(error, results){
+					response.send(JSON.stringify(results[0]));
+				}
+			);
+		}
+	);
+});
+
+router.get('/:object_id/getdescendents', function(request, response, next) {
+	getCollection(
+		function(collection){
+			console.log('got collection ' + collection);
+			collection.find({ 'id': request.params.object_id }).toArray(
+				function(error, results){
+					response.send(JSON.stringify(results[0]));
+				}
+			);
+		}
+	);
 });
 
 module.exports = router;
