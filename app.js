0bae6634c76b6364558346785d4781266a8ae328
stevethehat <stevethehat@gmail.com>
2015-06-26 15:43:55 +0100

    added support for DELETE

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   next();
 });
 
