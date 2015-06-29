72390b859d5427ab2e629a59ea3bfd89f04d0c17
stevethehat <stevethehat@gmail.com>
2015-06-29 18:21:14 +0100

    added palette item & icon controls

--- app/js/app/app-ui-controls.js
+++ app/js/app/app-ui-controls.js
@@ -1,6 +1,26 @@
 "use strict";
 var ZEN = (typeof ZEN === undefined) ? {} : ZEN;
 
+function ControlGroupControl (params, parent) {
+    if (arguments.length > 0) {
+		ZEN.ui.View.call(this, params, parent);
+    }
+    return this;
+}
+ControlGroupControl.prototype = new ZEN.ui.View();
+_.extend(
+	ControlGroupControl.prototype,
+		{
+			init: function (params, parent) {
+				// call the base class init method
+				ZEN.ui.View.prototype.init.call(this, params, parent);
+			},
+			render: function (params) {
+				var div = $('<div class="controlGroup"/>').appendTo(this.el);
+			}
+		}
+);
+
 function PaletteItemControl (params, parent) {
     if (arguments.length > 0) {
 		ZEN.ui.View.call(this, params, parent);
@@ -18,7 +38,7 @@ _.extend(
 			render: function (params) {
 				ZEN.log('creating pelette item');
 				if(params.icon != undefined){
-					var div = $('<div class="paletteItem fa ' + params.icon + ' fa-2x"/>').appendTo(this.el);
+					var div = $('<div class="paletteItem fa ' + params.icon + ' fa-1x"/>').appendTo(this.el);
 					
 					$('<span/>').text(params.content).appendTo(div); 	
 				} else {
@@ -43,11 +63,11 @@ _.extend(
 				ZEN.ui.View.prototype.init.call(this, params, parent);
 			},
 			render: function (params) {
-				var div = $('<div class="icon fa ' + params.icon + ' fa-2x"/>').appendTo(this.el);
+				var div = $('<div class="icon fa ' + params.icon + ' fa-1x"/>').appendTo(this.el);
 			}
 		}
 );
 
-
+ZEN.registerType('ControlGroup', ControlGroupControl);
 ZEN.registerType('PaletteItem', PaletteItemControl);
 ZEN.registerType('Icon', IconControl);
